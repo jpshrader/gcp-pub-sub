0a4e62ec92772bef097ac03dce34f81615f2dcfc
Jack <96661942+jpshrader@users.noreply.github.com>
2023-09-20 23:57:40 -0400

    working pretty good

--- main.go
+++ main.go
@@ -2,54 +2,96 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/pubsub"
 )
 
+type pubSubConfig struct {
+	ProjectId           string
+	TopicId             string
+	DeadLetterTopicId   string
+	SubscriptionId      string
+	AckDeadline         time.Duration
+	ReceiveSettings     pubsub.ReceiveSettings
+	NumMessages         int
+	TimeBetweenMessages time.Duration
+	MaxDeliveryAttempts int
+	RetryPolicy         pubsub.RetryPolicy
+}
+
 func main() {
-	projectID := "gcp-sup-sub"
-	emulatorHost := "localhost:8085"
+	config := pubSubConfig{
+		ProjectId:           "gcp-pub-sub",
+		TopicId:             "topicId",
+		DeadLetterTopicId:   "deadletter-topicId",
+		SubscriptionId:      "subscriptionId",
+		AckDeadline:         15 * time.Second,
+		NumMessages:         2,
+		TimeBetweenMessages: 2 * time.Second,
+		MaxDeliveryAttempts: 10,
+		ReceiveSettings: pubsub.ReceiveSettings{
+			MaxOutstandingMessages: 0,
+			NumGoroutines:          1,
+			Synchronous:            true,
+			MaxExtension:           10 * time.Second,
+		},
+		RetryPolicy: pubsub.RetryPolicy{
+			MinimumBackoff: 3 * time.Second,
+			MaximumBackoff: 10 * time.Second,
+		},
+	}
 
-	// Set the environment variable to use the Pub/Sub emulator
-	os.Setenv("PUBSUB_EMULATOR_HOST", emulatorHost)
+	ctx := context.Background()
+	os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
 
-	// Create a client to interact with the Pub/Sub emulator
-	client, err := pubsub.NewClient(context.Background(), projectID)
+	client, err := pubsub.NewClient(ctx, config.ProjectId)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
+		log.Fatalf("failed to create client: %v", err)
 	}
 	defer client.Close()
 
-	topicID := "test-topic"
-	subscriptionID := "test-subscription"
-
-	// Create a topic
-	topic, err := createTopic(client, topicID)
+	topic, err := createTopic(ctx, client, config.TopicId)
 	if err != nil {
-		log.Fatalf("Failed to create topic: %v", err)
+		log.Fatalf("failed to create topic: %v", err)
+	}
+	deadletterTopic, err := createTopic(ctx, client, config.DeadLetterTopicId)
+	if err != nil {
+		log.Fatalf("failed to create deadletter topic: %v", err)
 	}
 
-	// Create a subscription with your desired settings
-	subscription, err := createSubscription(client, subscriptionID, topic, 30*time.Second, 10)
+	subscription, err := createSubscription(ctx, client, topic, deadletterTopic, config)
 	if err != nil {
-		log.Fatalf("Failed to create subscription: %v", err)
+		log.Fatalf("failed to create subscription: %v", err)
 	}
 
-	// Publish a message to the topic
-	message := "Hello, Pub/Sub!"
-	publishResult := publishMessage(topic, message)
-	fmt.Printf("Message published: %s\n", publishResult)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for i := 0; i < config.NumMessages; i++ {
+			message := strconv.Itoa(i)
+			err := publishMessage(ctx, topic, message)
+			if err != nil {
+				log.Fatalf("failed to publish message: %v", err)
+			}
+		}
+		log.Println("published all messages")
+	}()
 
-	// Receive and process messages from the subscription
-	receiveMessages(subscription)
+	wg.Add(1)
+	go func() {
+		receiveMessages(ctx, subscription, config)
+	}()
+
+	wg.Wait()
+	log.Println("done")
 }
 
-func createTopic(client *pubsub.Client, topicID string) (*pubsub.Topic, error) {
-	ctx := context.Background()
+func createTopic(ctx context.Context, client *pubsub.Client, topicID string) (*pubsub.Topic, error) {
 	topic := client.Topic(topicID)
 
 	exists, err := topic.Exists(ctx)
@@ -67,9 +109,8 @@ func createTopic(client *pubsub.Client, topicID string) (*pubsub.Topic, error) {
 	return topic, nil
 }
 
-func createSubscription(client *pubsub.Client, subscriptionID string, topic *pubsub.Topic, ackDeadline time.Duration, maxOutstandingMessages int) (*pubsub.Subscription, error) {
-	ctx := context.Background()
-	subscription := client.Subscription(subscriptionID)
+func createSubscription(ctx context.Context, client *pubsub.Client, topic *pubsub.Topic, dlTopic *pubsub.Topic, config pubSubConfig) (*pubsub.Subscription, error) {
+	subscription := client.Subscription(config.SubscriptionId)
 
 	exists, err := subscription.Exists(ctx)
 	if err != nil {
@@ -79,41 +120,58 @@ func createSubscription(client *pubsub.Client, subscriptionID string, topic *pub
 	if !exists {
 		cfg := pubsub.SubscriptionConfig{
 			Topic:       topic,
-			AckDeadline: ackDeadline,
+			AckDeadline: config.AckDeadline,
+			RetryPolicy: &config.RetryPolicy,
+			DeadLetterPolicy: &pubsub.DeadLetterPolicy{
+				DeadLetterTopic:     dlTopic.String(),
+				MaxDeliveryAttempts: config.MaxDeliveryAttempts,
+			},
 		}
-		subscription, err = client.CreateSubscription(ctx, subscriptionID, cfg)
+		subscription, err = client.CreateSubscription(ctx, config.SubscriptionId, cfg)
 		if err != nil {
 			return nil, err
 		}
-		subscription.ReceiveSettings = pubsub.ReceiveSettings{
-			MaxOutstandingMessages: maxOutstandingMessages,
-			NumGoroutines:          1,
-			Synchronous:            true,
-		}
+		subscription.ReceiveSettings = config.ReceiveSettings
 	}
 
 	return subscription, nil
 }
 
-func publishMessage(topic *pubsub.Topic, message string) string {
-	ctx := context.Background()
+func publishMessage(ctx context.Context, topic *pubsub.Topic, message string) error {
 	result := topic.Publish(ctx, &pubsub.Message{
 		Data: []byte(message),
 	})
-	id, err := result.Get(ctx)
+	_, err := result.Get(ctx)
 	if err != nil {
-		return fmt.Sprintf("Failed to publish: %v", err)
+		return err
 	}
-	return fmt.Sprintf("Published message with ID: %s", id)
+	return err
 }
 
-func receiveMessages(subscription *pubsub.Subscription) {
-	ctx := context.Background()
+func receiveMessages(ctx context.Context, subscription *pubsub.Subscription, config pubSubConfig) {
 	err := subscription.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
-		fmt.Printf("Received message: %s\n", string(msg.Data))
+		messageCount, err := strconv.ParseInt(string(msg.Data), 10, 64)
+		if err != nil {
+			log.Printf("error parsing message: %v", err)
+			msg.Ack()
+			return
+		}
+		deliveryAttempt := 1
+		if msg.DeliveryAttempt != nil {
+			deliveryAttempt = *msg.DeliveryAttempt
+		}
+
+		if messageCount%2 == 0 {
+			log.Printf("message: %d, attempt #%d ❌\n", messageCount, deliveryAttempt)
+			msg.Nack()
+			return
+		}
+
+		log.Printf("message: %d, attempt #%d ✅\n", messageCount, deliveryAttempt)
+		time.Sleep(config.TimeBetweenMessages)
 		msg.Ack()
 	})
 	if err != nil {
-		log.Printf("Error receiving messages: %v", err)
+		log.Printf("error receiving messages: %v", err)
 	}
 }
